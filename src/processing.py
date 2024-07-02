6f85f2184c263cca5ce4c1400119d72d90de72ae
Tagir-Alisheykhov <ahitahir@yandex.ru>
2024-07-02 04:35:41 +0300

    add func filter_by_state and test linnters

---
+++ src/processing.py
@@ -0,0 +1,30 @@
+# Пример входных данных
+test_dict_list = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+
+
+def filter_by_state(list_dict: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """
+    Принимает список словарей и опционально значение для ключа state (по умолчанию 'EXECUTED').
+    Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state
+    соответствует указанному значению.
+    """
+    new_list_dict = []
+    for dict_values in list_dict:
+        if state == "EXECUTED":
+            for value in dict_values.values():
+                if value == state:
+                    new_list_dict.append(dict_values)
+        elif state:
+            for value in dict_values.values():
+                if value == state:
+                    new_list_dict.append(dict_values)
+    return new_list_dict
+
+
+if __name__ == "__main__":
+    print(filter_by_state(test_dict_list))
