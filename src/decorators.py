10ef8df4083903b311b41ecff75ba42333b8c694
Tagir-Alisheykhov <ahitahir@yandex.ru>
2024-07-24 16:54:20 +0300

    modify /decorators.py and add modul /test_decorators

--- src/decorators.py
+++ src/decorators.py
@@ -1,48 +1,42 @@
-# Ожидаемый вывод в лог-файл mylog.txt
-# при успешном выполнении:
-# >>> my_function ok
-#
-# Ожидаемый вывод при ошибке:
-# >>> my_function error: тип ошибки. Inputs: (1, 2), {}
-#
-# ---> Где "тип ошибки" заменяется на текст ошибки.
-from functools import wraps
 import os
+from functools import wraps
+from typing import Any
 
-base_path = "C:/Users/Lenovo/SkyProLearn2/SkyProject_2/Homework_AlisheykhovTM/Homework_Alisheykhov_TM/"
-full_path_my_log = os.path.join(base_path + "logs", "my_log.txt")
+base_path = ("C:/Users/Lenovo/SkyProLearn2/SkyProject_2/Homework_AlisheykhovTM"
+             "/Homework_Alisheykhov_TM/"
+             "logs/")
 
 
-def log(filename=None):
+def log(filename: str = None) -> Any:
+    """Логирование данных"""
     def real_decorator(func):
         @wraps(func)
-        def wrapper(*args, **kwargs):
-            print(f"Декоратор успешно запущен.")
-            result = func(*args, **kwargs)
-            if filename:
-                with open(full_path_my_log, "a", encoding="UTF-8") as file:
-                    file.write(f"/{func.__name__} - Запись файла прошла успешно\n")
-            else:
-                print(f"{func.__name__} error: <Указать тип ошибки>. inputs: {args}, {kwargs}")
-            print(f"Конец работы декоратора.")
-            return result
+        def wrapper(*args: Any, **kwargs: Any):
+            res = func(*args, **kwargs)
+            try:
+                print("Декоратор успешно запущен.")
+                if filename == "":
+                    raise SyntaxError("Строка аргумента не должна быть пустой")
+                elif not filename:
+                    raise ValueError("Нет обязательного аргумента")
+                elif filename:
+                    with open(os.path.join(base_path + filename), "a", encoding="UTF-8") as file:
+                        file.write(f"/{func.__name__} - Запись файла прошла успешно\n")
+            except SyntaxError as err_1:
+                print(f"{func.__name__} error: {err_1}. inputs: {args}, {kwargs}")
+            except ValueError as err_2:
+                print(f"{func.__name__} error: {err_2}. inputs: {args}, {kwargs}")
+            print("Конец работы декоратора.")
+            return res
         return wrapper
     return real_decorator
 
 
 @log(filename="my_log.txt")
-def my_function(x, y):
-    """ Сложение чисел """
-    return x + y
+def my_function(digit1: int, digit2: int) -> int:
+    """Сложение чисел"""
+    return digit1 + digit2
 
 
-my_function(1, 2)
-
-# ТЕСТИРОВАНИЕ
-
-# 1 - РЕАЛИЗОВАТЬ ТЕСТИРОВАНИЕ НА "CAPSYS"
-# 2 - РЕАЛИЗОВАТЬ ТЕСТИРОВАНИЕ НА РАЗНЫЕ СЦЕНАРИИ И НЕПРАВИЛЬНЫЕ ДАННЫЕ
-# 3 - МОЖНО ДОБАВИТЬ ПРЕДИКАТ, ЕСЛИ НЕ НАПИСАН FILENAME
-# 4 - НЕКОРРЕКТНОЕ ЗНАЧЕНИЕ ВНУТРИ ФАЙЛ-НЕЙМ --> ТОЛЬКО СТРОКИ
-# 5 - УКАЗАТЬ ТИП ОШИБКИ В ЛОГЕ, ЕСЛИ ВЫВОД В КОНСОЛЬ
-# 6 - СДЕЛАТЬ "PULL-REQUEST"
+if __name__ == "__main__":
+    my_function(1, 2)
---
+++ tests/test_decorators.py
@@ -0,0 +1,47 @@
+from src.decorators import log
+
+
+@log(filename="")
+def func_for_test_1(digit1, digit2):
+    """Функция для теста 1"""
+    return digit1 + digit2
+
+
+@log(filename=None)
+def func_for_test_2(digit1, digit2):
+    """Функция для теста 2"""
+    return digit1 + digit2
+
+
+@log(filename="my_log.txt")
+def func_for_test_3(digit1, digit2):
+    """Функция для теста 3"""
+    return digit1 + digit2
+
+
+def test_my_func_output_1(capsys):
+    """Захват вывода в консоль. Вариант 1"""
+    func_for_test_1(1, 3)
+    out, err = capsys.readouterr()
+    assert out == ('Декоратор успешно запущен.\n'
+                   'func_for_test_1 error: Строка аргумента не должна быть пустой. inputs: (1, 3), {}\n'
+                   'Конец работы декоратора.\n')
+    assert err == ""
+
+
+def test_my_func_output_2(capsys):
+    """Захват вывода в консоль. Вариант 2"""
+    func_for_test_2(1, 3)
+    out, err = capsys.readouterr()
+    assert out == ('Декоратор успешно запущен.\n'
+                   'func_for_test_2 error: Нет обязательного аргумента. inputs: (1, 3), {}\n'
+                   'Конец работы декоратора.\n')
+    assert err == ""
+
+
+def test_my_func_output_3(capsys):
+    """Захват вывода в консоль. Вариант 2"""
+    func_for_test_3(1, 3)
+    out, err = capsys.readouterr()
+    assert out == 'Декоратор успешно запущен.\nКонец работы декоратора.\n'
+    assert err == ""
