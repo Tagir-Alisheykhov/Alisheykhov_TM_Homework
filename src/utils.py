91da45e495ee45a695fb3e01d0165faf3602fad8
Tagir-Alisheykhov <ahitahir@yandex.ru>
2024-08-08 00:32:00 +0300

    check new functional through linters

--- src/utils.py
+++ src/utils.py
@@ -1,32 +1,42 @@
-import os
 import json
+import os
 from os import path
+from typing import Any
+
 from dotenv import load_dotenv
+
 from src.external_api import currency_conversion
 
 load_dotenv()
-API_KEY = os.getenv('KEY_API')
+API_KEY = os.getenv("KEY_API")
 file_json = path.join(path.dirname(path.dirname(__file__)), "data/operations.json")
 
 
-def processing_json_file(filename: str) -> list[dict]:
+def processing_json_file(filename: str) -> Any:
     """Открытие j-son файла с транзакциями"""
-    with (open(filename, encoding="UTF-8") as transactions_file):
+    with open(filename, encoding="UTF-8") as transactions_file:
         transactions = json.load(transactions_file)
-        currency_current_transact = ""
         if transactions == "":
             raise ValueError("Передан пустой файл")
         for transact in transactions:
-            if ('id' not in transact and 'state' not in transact
-                    and 'operationAmount' not in transact and 'description' not in transact):
+            if (
+                "id" not in transact
+                and "state" not in transact
+                and "operationAmount" not in transact
+                and "description" not in transact
+            ):
                 continue
             else:
-                if ('id' not in transact or 'state' not in transact
-                        or 'operationAmount' not in transact or 'description' not in transact):
+                if (
+                    "id" not in transact
+                    or "state" not in transact
+                    or "operationAmount" not in transact
+                    or "description" not in transact
+                ):
                     raise KeyError("Отсутствует обязательный ключ в транзакции")
-                # else:
-                elif (len(str(transact['operationAmount']['currency']['code'])) != 3
-                        or not isinstance(transact['operationAmount']['currency']['code'], str)):
+                elif len(str(transact["operationAmount"]["currency"]["code"])) != 3 or not isinstance(
+                    transact["operationAmount"]["currency"]["code"], str
+                ):
                     raise ValueError("Неверный трехзначный код валюты в транзакции")
         return transactions
 
@@ -35,13 +45,13 @@ def sum_transactions(transactions_data: list[dict]) -> float:
     """Возвращает сумму всех транзакций в рублях"""
     list_currency = []
     list_amount = []
-    finish_amount = 0
-    received_currency = 'USD'
+    finish_amount = 0.0
+    received_currency = "USD"
     for transact_dicts in transactions_data:
-        currency_current_transact = transact_dicts['operationAmount']['currency']['code']
-        amount_current_transact = float(transact_dicts['operationAmount']['amount'])
-        if currency_current_transact == 'RUB':
-            finish_amount += amount_current_transact
+        currency_current_transact = transact_dicts["operationAmount"]["currency"]["code"]
+        amount_current_transact = float(transact_dicts["operationAmount"]["amount"])
+        if currency_current_transact == "RUB":
+            float(finish_amount) + amount_current_transact
         else:
             list_amount.append(amount_current_transact)
             list_currency.append(currency_current_transact)
@@ -52,11 +62,12 @@ def sum_transactions(transactions_data: list[dict]) -> float:
                     continue
     conversion = currency_conversion(received_currency)
     for amount_one_transact in list_amount:
-        finish_amount += (conversion * amount_one_transact)
+        multiply = conversion * amount_one_transact
+        finish_amount = finish_amount + multiply
     return round(finish_amount, 2)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     data_file = processing_json_file(file_json)
     sum_all_transacts = sum_transactions(data_file)
     print(sum_all_transacts)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,49 +1,54 @@
 import json
-import pytest
 from os import path
+from typing import Any
+
+import pytest
+
 from src.utils import processing_json_file, sum_transactions
 
-
-invalid_keys_file_json = path.join(path.dirname(path.dirname(__file__)), "data_for_tests/"
-                                                                         "invalid_keys_in_operation.json")
-invalid_code_currency_json = path.join(path.dirname(path.dirname(__file__)), "data_for_tests/"
-                                                                             "invalid_code_currency_in_operation.json")
-invalid_empty_file = path.join(path.dirname(path.dirname(__file__)), "data_for_tests/"
-                                                                     "invalid_code_currency_in_operation.json")
+invalid_keys_file_json = path.join(
+    path.dirname(path.dirname(__file__)), "data_for_tests/" "invalid_keys_in_operation.json"
+)
+invalid_code_currency_json = path.join(
+    path.dirname(path.dirname(__file__)), "data_for_tests/" "invalid_code_currency_in_operation.json"
+)
+invalid_empty_file = path.join(
+    path.dirname(path.dirname(__file__)), "data_for_tests/" "invalid_code_currency_in_operation.json"
+)
 mini_file_json = path.join(path.dirname(path.dirname(__file__)), "data_for_tests/mini_operations.json")
 
 
-def open_file_with_tests_transactions() -> list[dict]:
+def open_file_with_tests_transactions() -> Any:
     """Данные для теста корректности работы функции processing_json_file"""
-    with open(mini_file_json, encoding='UTF-8') as f:
+    with open(mini_file_json, encoding="UTF-8") as f:
         json_file = json.load(f)
     return json_file
 
 
-def test_processing_json_file():
+def test_processing_json_file() -> None:
     """Проверка на корректность работы функции processing_json_file"""
     assert processing_json_file(mini_file_json) == filtered_data
 
 
-def test_invalid_key_in_processing_json_file():
+def test_invalid_key_in_processing_json_file() -> None:
     """Проверка вызова исключения при отсутствии обязательного ключа"""
     with pytest.raises(KeyError):
         assert processing_json_file(invalid_keys_file_json)
 
 
-def test_invalid_code_currency_in_processing_json_file():
+def test_invalid_code_currency_in_processing_json_file() -> None:
     """Проверка вызова исключения при неправильном коде валюты"""
     with pytest.raises(ValueError):
         assert processing_json_file(invalid_code_currency_json)
 
 
-def test_invalid_empty_in_processing_json_file():
+def test_invalid_empty_in_processing_json_file() -> None:
     """Проверка вызова исключения при неправильном коде валюты"""
     with pytest.raises(ValueError):
         assert processing_json_file(invalid_empty_file)
 
 
-def test_sum_transactions():
+def test_sum_transactions() -> None:
     """Проверка корректности работы функции sum_transactions"""
     assert sum_transactions(filtered_data) == result_sum_all_operations
 
