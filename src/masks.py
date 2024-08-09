132e21e6ed9587abf011ec86926a637645ab94e1
Tagir-Alisheykhov <ahitahir@yandex.ru>
2024-08-09 16:13:46 +0300

    Revert "Feature/task 5"

--- src/masks.py
+++ src/masks.py
@@ -11,39 +11,25 @@
 """
 
 
-def get_mask_card_number(cart_numb: str | int) -> str:
+def get_mask_card_number(cart_numb: str) -> str:
     """Возвращает номер карты с маской"""
-    cart_numb_str = str(cart_numb)
-    if cart_numb == "":
-        raise ValueError("Нет данных")
-    if not cart_numb_str.isdigit():
-        raise ValueError("Номер должен состоять только из цифр")
-    if len(cart_numb_str) != 16:
-        raise ValueError("Неверный номер карты")
     new_list = list()
-    new_list.append(cart_numb_str[0:4])
-    new_list.append(cart_numb_str[4:6] + "**")
+    new_list.append(cart_numb[0:4])
+    new_list.append(cart_numb[4:6] + "**")
     new_list.append("****")
-    new_list.append(cart_numb_str[12:])
+    new_list.append(cart_numb[12:])
     return " ".join(new_list)
 
 
 def get_mask_account(account_number: str) -> str:
     """Возвращает номер счета с маской"""
-    account_number_str = str(account_number)
-    if account_number == "":
-        raise ValueError("Нет данных")
-    if not account_number_str.isdigit():
-        raise ValueError("Номер должен состоять только из цифр")
-    if len(account_number_str) != 20:
-        raise ValueError("Неверный номер счета")
     new_list = list()
-    new_list.append("**" + account_number_str[-4:])
+    new_list.append("**" + account_number[-4:])
     return " ".join(new_list)
 
 
 # Входные данные и их вывод для теста функций
-# client_cart_number = 7000791119606365
-# client_account_number = "73654108430135874305"
-# # print(get_mask_card_number(str(client_cart_number)))
+# client_account_number = 73654108430135874305
+# client_cart_number = 7000791119606361
 # print(get_mask_account(str(client_account_number)))
+# print(get_mask_card_number(str(client_cart_number)))
