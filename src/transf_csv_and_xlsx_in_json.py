44646d2fe1aa958a58db5b80f49ef2ba71f9a968
Tagir-Alisheykhov <ahitahir@yandex.ru>
2024-08-27 22:08:17 +0300

    install lib pandas-stubs and check linters

--- src/transf_csv_and_xlsx_in_json.py
+++ src/transf_csv_and_xlsx_in_json.py
@@ -5,16 +5,21 @@
 """
 
 import json
+from os import path
+from typing import Any
 import pandas as pd
 
+file_csv = path.join(path.dirname(path.dirname(__file__)), "data/")
+file_xlsx = path.join(path.dirname(path.dirname(__file__)), "data/")
 
-def get_csv_and_change_json(file_csv: pd.DataFrame, file_xlsx: pd.DataFrame) -> json:
+
+def get_csv_and_change_json(csv: pd.DataFrame, xlsx: pd.DataFrame) -> Any:
     """Преобразовывает файлы csv и xlsx и создает json-файлы"""
-    file_csv.to_json('data/csv_in_js.json', orient='records', indent=4)
-    file_xlsx.to_json('data/xlsx_in_js.json', orient='records', indent=4)
+    csv.to_json(file_csv + "csv_in_js.json", orient="records", indent=4)
+    xlsx.to_json(file_xlsx + "xlsx_in_js.json", orient="records", indent=4)
 
 
-if __name__ == '__main__':
-    df_csv = pd.read_csv("data/transactions.csv", delimiter=';', encoding='UTF-8')
-    df_xlsx = pd.read_excel("data/transactions_excel.xlsx")
+if __name__ == "__main__":
+    df_csv = pd.read_csv(file_csv + "transactions.csv", delimiter=";", encoding="UTF-8")
+    df_xlsx = pd.read_excel(file_xlsx + "transactions_excel.xlsx")
     write_new_json_files = get_csv_and_change_json(df_csv, df_xlsx)
